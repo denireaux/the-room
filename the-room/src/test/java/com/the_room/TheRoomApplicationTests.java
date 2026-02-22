2156505da4671a5975bbe9a7315be2fe18ff9c5a
denireaux <dangelo.l.deniro@gmail.com>
2026-02-22 13:26:14 -0700

    Springboot/docker setup

---
+++ the-room/src/test/java/com/the_room/TheRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.the_room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
